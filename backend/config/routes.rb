cde4ebe13cc5b72e541140017561757e93595731
JuanDavidDV <juandadazav@gmail.com>
2025-05-21 21:53:02 -0400

    add namespace in routes file

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  resources :todos
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  namespace :api do   # namespace makes: /api/todos
+    resources :todos do # setup endpoints for todos within /api
+      member do # Used because update_completed is not inside the scaffold. Path will look like: /api/todos/:id/update_completed
+        patch "update_completed"
+      end
+    end
+  end
 end
