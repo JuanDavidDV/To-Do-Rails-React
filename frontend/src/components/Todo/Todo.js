03215bcb37906a1ac9d7a0b991fed19103215b0d
JuanDavidDV <juandadazav@gmail.com>
2025-05-22 08:27:28 -0400

    add props

--- frontend/src/components/Todo/Todo.js
+++ frontend/src/components/Todo/Todo.js
@@ -1,12 +1,12 @@
 import "./Todo.css"
 import { MdDeleteForever } from "react-icons/md";
 
-const Todo = () => {
+const Todo = ( {todo_list} ) => {
   return (
     <div className= "todo"> 
       <div className="todo-container">
         <input type="checkbox" className="todo-checkbox"/>
-          <h3 className="todo-title">Task 1</h3>          
+          <h3 className="todo-title"> {todo_list} </h3>          
           <MdDeleteForever size="1.75rem" className="delete-icon" />
       </div>
     </div>
--- frontend/src/components/TodoList/TodoList.js
+++ frontend/src/components/TodoList/TodoList.js
@@ -4,9 +4,9 @@ import "./TodoList.css"
 const TodoList = () => {
  return (
   <div className="todo-list">
-    <Todo />
-    <Todo />
-    <Todo />
+    <Todo todo_list="Task 1" />
+    <Todo todo_list="Task 2"/>
+    <Todo todo_list="Task 3"/>
   </div>
  )
 };
