235999effaad39db283f30cccf7cf9cf4f46f4e4
JuanDavidDV <juandadazav@gmail.com>
2025-05-22 10:58:03 -0400

    style add todo UI

--- frontend/src/components/AddTodo/AddTodo.js
+++ frontend/src/components/AddTodo/AddTodo.js
@@ -1,5 +1,12 @@
-const AddTodo = () => {
+import "./AddTodo.css";
 
+const AddTodo = ( {addTodo} ) => {
+  return (
+    <div className="add-todo">
+      <input type="text" className="add-todo-input" />
+      <button className="add-todo-button">Add Todo</button>
+    </div>
+  )
 }
 
 export default AddTodo;
