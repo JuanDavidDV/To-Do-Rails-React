f1fcd6a18436a1902e2d7a7247a6a7df3a2af840
JuanDavidDV <juandadazav@gmail.com>
2025-05-22 11:28:23 -0400

    create useState and handleAdd functions

--- frontend/src/components/AddTodo/AddTodo.js
+++ frontend/src/components/AddTodo/AddTodo.js
@@ -1,10 +1,19 @@
 import "./AddTodo.css";
+import {useState} from "react";
 
 const AddTodo = ( {addTodo} ) => {
+
+  const [todoInput, setTodoInput] = useState("");
+
+  const handleAdd = () => {
+    addTodo(todoInput);
+    setTodoInput(""); 
+  }
+
   return (
     <div className="add-todo">
-      <input type="text" className="add-todo-input" />
-      <button className="add-todo-button">Add Todo</button>
+      <input onChange={(e) => setTodoInput(e.target.value)} type="text" className="add-todo-input" />
+      <button onClick={handleAdd} className="add-todo-button">Add Todo</button>
     </div>
   )
 }
