6f6b746f3d0c0cdf009b9b918360ccf2583ba07c
JuanDavidDV <juandadazav@gmail.com>
2025-05-21 21:54:12 -0400

    remove space

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
       member do # Used because update_completed is not inside the scaffold. Path will look like: /api/todos/:id/update_completed
         patch "update_completed"
       end
-    end 
+    end
   end
 end
