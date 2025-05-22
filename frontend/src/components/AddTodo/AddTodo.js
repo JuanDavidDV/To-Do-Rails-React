5e301b99be4384634abb8a1b7115b6db74c463d4
JuanDavidDV <juandadazav@gmail.com>
2025-05-22 15:18:28 -0400

    finished application

--- frontend/src/components/AddTodo/AddTodo.js
+++ frontend/src/components/AddTodo/AddTodo.js
@@ -1,19 +1,30 @@
+import { MdFaceRetouchingNatural } from "react-icons/md";
 import "./AddTodo.css";
 import {useState} from "react";
 
 const AddTodo = ( {addTodo} ) => {
 
   const [todoInput, setTodoInput] = useState("");
+  const [error, setError] = useState("");
 
   const handleAdd = () => {
+    if (todoInput.trim() === "") {
+      setError("Please enter a task");
+      return;
+    }
+
     addTodo(todoInput);
     setTodoInput(""); 
-  }
+    setError("");
+  };
 
   return (
     <div className="add-todo">
-      <input onChange={(e) => setTodoInput(e.target.value)} type="text" className="add-todo-input" />
-      <button onClick={handleAdd} className="add-todo-button">Add Todo</button>
+      {error && <p className="add-todo-error">{error}</p>}
+      <div className="add-todo-container">
+        <input onChange={(e) => setTodoInput(e.target.value)} value={todoInput} type="text" className="add-todo-input" />
+        <button onClick={handleAdd} className="add-todo-button">Add Todo</button>
+      </div>
     </div>
   )
 }
