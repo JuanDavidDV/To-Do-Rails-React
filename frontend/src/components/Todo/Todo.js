63b79fe4567e8587b129b35adf7eea15798531c5
JuanDavidDV <juandadazav@gmail.com>
2025-05-22 08:09:49 -0400

    update UI style

--- frontend/src/components/Todo/Todo.js
+++ frontend/src/components/Todo/Todo.js
@@ -7,7 +7,7 @@ const Todo = () => {
       <div className="todo-container">
         <input type="checkbox" className="todo-checkbox"/>
           <h3 className="todo-title">Task 1</h3>          
-          <MdDeleteForever size="2rem" color="red" />
+          <MdDeleteForever size="1.75rem" className="delete-icon" />
       </div>
     </div>
   )
